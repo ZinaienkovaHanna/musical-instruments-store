52b5ee9e9e1a5ab206540e37d3d74283147ad23b
mmcxph <mmcxph@gmail.com>
2022-08-13 08:36:23 +0000

    carousel changed

--- js/js-carousel-adv.js
+++ js/js-carousel-adv.js
@@ -1,20 +1,74 @@
-  $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:true
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    dots: false,
+    margin: 10,
+    stagePadding: 50,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
         },
-        600:{
-            items:3,
-            nav:false
+        768: {
+            items: 2,
+            // nav:false
         },
-        1000:{
-            items:5,
-            nav:true,
-            loop:false
-        }
+        992: {
+            items: 3,
+            // nav:false
+        },
+        1000: {
+            items: 6,
+            // nav:true,
+            loop: false
+        },
+        nav: true,
+        navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>', '<i class="fa fa-angle-right" aria-hidden="true"></i>'],
     }
-})
+});
+
+
+// $('.owl-carousel').owlCarousel({
+//     loop: true,
+//     margin: 10,
+//     stagePadding: 30,
+//     nav: true,
+//     dots: false,
+//     responsiveClass: true,
+//     responsive: {
+//         0: {
+//             items: 1,
+//             nav: true
+//         },
+//         768: {
+//             items: 2,
+//             // nav:false
+//         },
+//         992: {
+//             items: 3,
+//             // nav:false
+//         },
+//         1000: {
+//             items: 6,
+//             // nav:true,
+//             loop: false
+//         }
+//     },
+
+
+//     navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>']
+
+    // navText: [
+    //     '<span class="arrow-owl arrow-left"></span>',
+    //     '<span class="arrow-owl arrow-right"></span>'
+    //   ],
+
+
+
+// $(".next_button").click(function () {
+//     owl.trigger("next.owl.carousel");
+// });
+
+// $(".prev_button").click(function () {
+//     owl.trigger("prev.owl.carousel");
+// });
