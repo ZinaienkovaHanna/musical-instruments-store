b0e6e58107628c7d93dad78d7a71f0603c0480aa
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2022-08-24 18:40:43 +0000

    added code in header-burger.js

--- js/header-burger.js
+++ js/header-burger.js
@@ -19,5 +19,17 @@
     popup.appendChild(navigationWrapper);
   }
 
+  const links = Array.from(navigationWrapper.children);
+
+  links.forEach((link) => {
+    link.addEventListener("click", closeOnClick);
+  });
+
+  function closeOnClick() {
+    popup.classList.remove("open");
+    burger.classList.remove("active");
+    body.classList.remove("noscroll");
+  }
+
   })();
   
