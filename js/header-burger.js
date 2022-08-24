ac0ca774e1ca0fc4656ac5b991b551a62960b79d
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2022-08-24 14:11:24 +0000

    added js, css in header

--- js/header-burger.js
+++ js/header-burger.js
@@ -3,12 +3,15 @@
   const burger = document.querySelector("#burger");
   const popup = document.querySelector("#popup");
   const navigationWrapper = document.querySelector("#navigation-wrapper").cloneNode(1);
+  const body = document.body;
 
   burger.addEventListener("click", burgerHandler);
 
   function burgerHandler(e) {
     e.preventDefault();
-    popup.classList.toggle("open")
+    popup.classList.toggle("open");
+    burger.classList.toggle("active");
+    body.classList.toggle("noscroll");
     renderPopup();
   }
 
